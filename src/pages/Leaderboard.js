5a2cb2d08697323dce6e014732875d01c24e87f0
exsersewo <exsersewo@systemexit.co.uk>
2020-04-15 23:39:08 +0100

    Add Compact Toggle & Update Experience Calc

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef, useCallback } from "react";
 import ReactDOM from "react-dom";
 import getLeaderboard from "../utilities/getLeaderboard";
 import LeaderboardEntry from "../components/LeaderboardEntry";
+import Toggle from "react-toggle";
+import "react-toggle/style.css";
 
 export default function Leaderboard(props) {
   const doMoneyCheck = function doMoneyCheck(props) {
@@ -22,6 +24,7 @@ export default function Leaderboard(props) {
 
   const [isMoney] = useState(doMoneyCheck(props));
   const [canLoadMore, setCanLoadMore] = useState(true);
+  const [compact, setCompact] = useState(false);
 
   const { entries, nextOffset, hasMore, loading, setURL } = getLeaderboard(
     props
@@ -72,8 +75,62 @@ export default function Leaderboard(props) {
     setCanLoadMore(false);
   }, [props, canLoadMore, setURL, setCanLoadMore, nextOffset]);
 
+  let toggleCompact = function () {
+    var toggle = document.getElementById("compact-toggle");
+    setCompact(toggle.checked);
+    if (toggle.checked) {
+      var box = document.getElementsByClassName("leaderboard-box");
+      for (var item in box.length) {
+        if (
+          box[item] !== undefined &&
+          !box[item].classList.contains("compact")
+        ) {
+          box[item].classList.add("compact");
+        }
+      }
+      var avs = document.getElementsByClassName("leaderboard-avatar");
+      for (var item in avs.length) {
+        if (
+          avs[item] !== undefined &&
+          !avs[item].classList.contains("compact")
+        ) {
+          avs[item].classList.add("compact");
+        }
+      }
+      var names = document.getElementsByClassName("leaderboard-name");
+      for (var item in names.length) {
+        if (
+          names[item] !== undefined &&
+          !names[item].classList.contains("compact")
+        ) {
+          names[item].classList.add("compact");
+        }
+      }
+    } else {
+      var compacts = document.getElementsByClassName("compact");
+
+      for (var item in compacts.length) {
+        if (
+          compacts[item] !== undefined &&
+          compacts[item].classList.contains("compact")
+        ) {
+          compacts[item].classList.remove("compact");
+        }
+      }
+    }
+  };
+
   return (
     <div id="content">
+      <div className="compact-toggle">
+        <div>Compact</div>
+        <Toggle
+          id="compact-toggle"
+          defaultChecked={false}
+          onClick={toggleCompact}
+          name="Compact"
+        />
+      </div>
       <div id="leaderboard" className="flexList">
         {entries.map((userentry, key) => {
           if (entries.length === key + 1) {
@@ -84,6 +141,7 @@ export default function Leaderboard(props) {
                 data={userentry}
                 position={key + 1}
                 isMoney={isMoney}
+                isCompact={compact}
               />
             );
           } else {
@@ -93,6 +151,7 @@ export default function Leaderboard(props) {
                 data={userentry}
                 position={key + 1}
                 isMoney={isMoney}
+                isCompact={compact}
               />
             );
           }
